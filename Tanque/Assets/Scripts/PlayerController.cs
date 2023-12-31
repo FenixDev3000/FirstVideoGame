89ed85b8951bc3114e34a96b2272c88f90324260
FenixDev3000 <154649566+FenixDev3000@users.noreply.github.com>
2023-12-31 16:24:11 -0500

    Add Camera y modificacion script playerController
    Añadi una camara secundaria y agregue nuevas lineas de codigo en playerController

--- Tanque/Assets/Scripts/PlayerController.cs
+++ Tanque/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,15 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float forwardInput;
 
+    //Variables de camara
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
+    //Variables multijugador
+    public string inputID;
+
+    
 
     void Start()
     {
@@ -18,13 +27,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Asignacion de configuraciones teclado
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+
+        
         //Movilidad de vehiculo
         transform.Translate( Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-        
-        
+
+        //Cambio entre camaras
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !mainCamera.enabled;
+        }
 
     }
 }
